822607548aecbf000d55b9b200235dfed40a462f
Nikita Makarin <zn.makarin@gmail.com>
2020-05-16 22:06:47 +0300

    Initial commit

---
+++ terminal-craft/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  terminal-craft
+//
+//  Created by Nikita Makarin on 16.05.2020.
+//  Copyright © 2020 Nikita Makarin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
